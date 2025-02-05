eacaed12778c90c87e6cff91c391a782c9a06a8b
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-04 23:27:51 -0300

    testando interpolação e jsx

--- novo-projeto/src/App.js
+++ novo-projeto/src/App.js
@@ -2,10 +2,13 @@
 import './App.css';
 
 function App() {
+
+  const name = "Rusthen"
+
   return (
     <div className="App">
      <h2>Alterando o jsx</h2>
-     <h2></h2>
+     <p>Olá , {name.toUpperCase()}</p>
     </div>
   );
 }
