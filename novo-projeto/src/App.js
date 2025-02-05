84b2dc01b56e36845e0631a9f2f9458cd5222c09
Rusthen Ronald <166277221+RusthenRonald@users.noreply.github.com>
2025-02-04 23:40:47 -0300

    declarando imagem

--- novo-projeto/src/App.js
+++ novo-projeto/src/App.js
@@ -5,10 +5,18 @@ function App() {
 
   const name = "Rusthen"
 
+  function sum(a,b){
+    return a+b
+  }
+
+  const url ='https://placeholder.com/150'
+  
+
   return (
     <div className="App">
      <h2>Alterando o jsx</h2>
      <p>Olá , {name.toUpperCase()}</p>
+     <p>Soma: {sum(2,4)}</p>
     </div>
   );
 }
